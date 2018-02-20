f707129aaf15d1049fb34ae276d43bb16a618fe9
Farzad YZ <farskid@gmail.com>
2018-02-20 10:01:19 +0300

    Adds utility functions

---
+++ utils/is-number.js
@@ -0,0 +1,15 @@
+
+/**
+ * @function isNumber
+ * @description Detects if the input is of type number
+ * @param {*} num
+ * @returns true if the input is a valid number, false otherwise
+ * @example
+ *  isNumber(Infinity) false
+ *  isNumber(2) true
+ *  isNumber(NaN) false
+ *  isNumber('hello world') false
+ */
+module.exports.isNumber = function isNumber(num) {
+	return typeof num === 'number' && isFinite(num) && !isNaN(num);
+}
---
+++ utils/is-positive.js
@@ -0,0 +1,15 @@
+const { isNumber } = require("./is-number");
+
+/**
+ * @function isPositive
+ * @description detects whether the input is number and positive
+ * @param {*} num
+ * @returns {boolean} true if the number is postive, false otherwise
+ * @example
+ *  isPositive(2) true
+ *  isPositive(-1) false
+ *  isPositive(0) false
+ */
+module.exports.isPositive = function isPositive(num) {
+  return isNumber(num) && num > 0;
+};
